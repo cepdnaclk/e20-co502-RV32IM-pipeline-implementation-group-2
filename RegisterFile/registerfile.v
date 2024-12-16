f0b06b2a26216983a7a63a580b5fc511ab87514d
Rusiru Induwara <150885221+induwara23630@users.noreply.github.com>
2024-12-16 13:31:35 +0530

    correct register

--- RegisterFile/registerfile.v
+++ RegisterFile/registerfile.v
@@ -67,10 +67,38 @@ module RegisterFile (RS1,RS2,WRITEDATA,WRITEADDRESS,WRITEENABLE,RESET,CLK,DATA1,
         if(RESET)
         begin
             #1
-            for (i = 0;i<32 ;i++ )
-            begin
-            registers[i] <= 0;
-            end 
+            registers[1] <= 0;
+            registers[2] <= 0;
+            registers[3] <= 0;
+            registers[4] <= 0;
+            registers[5] <= 0;
+            registers[6] <= 0;
+            registers[7] <= 0;
+            registers[8] <= 0;
+            registers[9] <= 0;
+            registers[10] <= 0;
+            registers[11] <= 0;
+            registers[12] <= 0;
+            registers[13] <= 0;
+            registers[14] <= 0;
+            registers[15] <= 0;
+            registers[16] <= 0;
+            registers[17] <= 0;
+            registers[18] <= 0;
+            registers[19] <= 0;
+            registers[20] <= 0;
+            registers[21] <= 0;
+            registers[22] <= 0;
+            registers[23] <= 0;
+            registers[24] <= 0;
+            registers[25] <= 0;
+            registers[26] <= 0;
+            registers[27] <= 0;
+            registers[28] <= 0;
+            registers[29] <= 0;
+            registers[30] <= 0;
+            registers[31] <= 0;
+            
         end
         else if(WRITEENABLE && WRITEADDRESS != 5'd0)
         begin
