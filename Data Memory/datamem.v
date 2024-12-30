90dda5b8484f30c56acd8a2249a0701d4d00621b
Rusiru Induwara <150885221+induwara23630@users.noreply.github.com>
2024-12-30 13:22:47 +0530

    dmem new

--- Data Memory/datamem.v
+++ Data Memory/datamem.v
@@ -9,14 +9,19 @@ output reg [31:0] READDATA;
 
 // Declare memory array 1024 x 8-bits 
 reg [7:0] memory_array [0:1023];
+integer i;
 
 //Reading & writing
-always @(posedge CLK)
+always @(posedge CLK or posedge RESET)
 begin
-    if(READ)
-    begin
-        READDATA = {memory_array[ADDRESS],memory_array[ADDRESS+1],memory_array[ADDRESS+2],memory_array[ADDRESS+3]};
+    //reset memory
+    if (RESET) begin
+        for (i = 0; i < 1024; i = i + 1) begin
+            memory_array[i] <= 8'b0; // Clear memory byte-by-byte
+        end
     end
+
+    // Write to memory
     if(WRITE)
 	begin
         memory_array[ADDRESS]   <= WRITEDATA[7:0];    
@@ -26,16 +31,17 @@ begin
     end
 end
 
-integer i;
 
-//Reset memory
-always @(posedge RESET)
-begin
-    if (RESET)
-    begin
-        for (i=0;i<1024; i=i+1)
-            memory_array[i] <= 0;
+always @(*) begin
+    if (READ) begin
+        READDATA = {memory_array[ADDRESS],      // LSB
+                    memory_array[ADDRESS + 1],  // Byte 1
+                    memory_array[ADDRESS + 2],  // Byte 2
+                    memory_array[ADDRESS + 3]}; // MSB
+        end 
+        else begin
+            READDATA = 32'b0; // Default value when READ is not enabled
+        end
     end
-end
 
 endmodule
