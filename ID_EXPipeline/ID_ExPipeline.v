6f3fc9001ec967bcd3b37a4b4d1dfe39f58d500d
samadhisakunika <150885396+samadhisakunika@users.noreply.github.com>
2025-02-05 18:37:40 +0530

    Forwading step1

--- ID_EXPipeline/ID_ExPipeline.v
+++ ID_EXPipeline/ID_ExPipeline.v
@@ -1,9 +1,11 @@
 module ID_ExPipeline (CLK,Reset,Write_Enable,Memory_Access,Mem_Write,Mem_Read,Jump_and_Link,ALU_Opcode,Immediate_Select,Offset_Generate,Branch,Jump,PC,PC_next,Data1,Data2,instruction,Immediate_value,Out_Write_Enable,Out_Memory_Access,Out_Mem_Write,Out_Mem_Read,Out_Jump_and_Link,Out_ALU_Opcode,Out_Immediate_Select,Out_Offset_Generate,Out_Branch,Out_Jump,Out_PC,Out_PC_next,Out_Data1,Out_Data2,Out_WriteAddress,Out_func3,Out_Immediate_value);
-    input Write_Enable,Memory_Access,Mem_Write,Mem_Read,Jump_and_Link,Immediate_Select,Offset_Generate,Branch,Jump;
+    input Write_Enable,Memory_Access,Mem_Write,Mem_Read,Jump_and_Link,Branch,Jump;
+    input [1:0] Immediate_Select,Offset_Generate;
     input[4:0] ALU_Opcode;
     input[31:0] PC,PC_next,Data1,Data2,instruction,Immediate_value;
     input CLK,Reset;
-    output reg Out_Write_Enable,Out_Memory_Access,Out_Mem_Write,Out_Mem_Read,Out_Jump_and_Link,Out_Immediate_Select,Out_Offset_Generate,Out_Branch,Out_Jump;
+    output reg Out_Write_Enable,Out_Memory_Access,Out_Mem_Write,Out_Mem_Read,Out_Jump_and_Link,Out_Branch,Out_Jump;
+    output reg[1:0] Out_Immediate_Select,Out_Offset_Generate;
     output reg[4:0] Out_ALU_Opcode;
     output reg[31:0] Out_PC,Out_PC_next,Out_Data1,Out_Data2,Out_Immediate_value;
     output reg[4:0] Out_WriteAddress;
@@ -17,8 +19,8 @@ module ID_ExPipeline (CLK,Reset,Write_Enable,Memory_Access,Mem_Write,Mem_Read,Ju
             Out_Mem_Write <= 1'bx;
             Out_Mem_Read <= 1'bx;
             Out_Jump_and_Link <= 1'bx;
-            Out_Immediate_Select <= 1'bx;
-            Out_Offset_Generate <= 1'bx;
+            Out_Immediate_Select <= 2'bx;
+            Out_Offset_Generate <= 2'bx;
             Out_Branch <= 1'bx;
             Out_Jump <= 1'bx;
             Out_ALU_Opcode <= 5'bx;
