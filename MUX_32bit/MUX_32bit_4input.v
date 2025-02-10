a4d05c814b403fe83c7a1d1c6012468f92607d7d
samadhisakunika <150885396+samadhisakunika@users.noreply.github.com>
2025-02-10 13:58:53 +0530

    forwarding_new

--- MUX_32bit/MUX_32bit_4input.v
+++ MUX_32bit/MUX_32bit_4input.v
@@ -1,5 +1,5 @@
-module MUX_32bit_3input(INPUT_0,INPUT_1,INPUT_2,SELECT,OUTPUT);
-input [31:0] INPUT_0,INPUT_1,INPUT_2;
+module MUX_32bit_4input(INPUT_0,INPUT_1,INPUT_2,INPUT_3,SELECT,OUTPUT);
+input [31:0] INPUT_0,INPUT_1,INPUT_2,INPUT_3;
 input [1:0] SELECT;
 output reg [31:0] OUTPUT;
 
@@ -9,6 +9,9 @@ always @(INPUT_0,INPUT_1,SELECT) begin
     end
     else if(SELECT == 2'b11) begin
         OUTPUT = INPUT_2;
+    end
+    else if(SELECT == 2'b01) begin
+        OUTPUT = INPUT_3;
     end
     else begin
         OUTPUT =INPUT_1;
